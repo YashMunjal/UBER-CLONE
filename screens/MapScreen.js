8924ce0c346d3ec70e0f001e38a8b4f6ccb52fab
yashmunjal2 <91688280+yashmunjal2@users.noreply.github.com>
2021-10-11 23:39:07 +0530

    Update MapScreen.js

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -5,6 +5,7 @@ const MapScreen = () => {
     return (
         <View>
             <Text>Here is the map ...</Text>
+            <Text> To load the map we may need the GCP cloud which we currently dont have</Text>
         </View>
     )
 }
